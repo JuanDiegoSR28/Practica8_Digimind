e58a6ed5860d56cc1489c1b758657004604ccaa1
JuanDiegoSR28 <juan.sanchez233013@potros.itson.edu.mx>
2024-06-18 21:55:29 -0700

    Digimind 2

---
+++ app/src/main/java/sanchez/juan/mydigimind/ui/Task.kt
@@ -0,0 +1,3 @@
+package sanchez.juan.mydigimind.ui
+
+data class Task(var title: String, var days: ArrayList<String>,var time:String)
--- app/src/main/java/sanchez/juan/mydigimind/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/sanchez/juan/mydigimind/ui/dashboard/DashboardFragment.kt
@@ -1,15 +1,31 @@
 package sanchez.juan.mydigimind.ui.dashboard
 
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import sanchez.juan.mydigimind.R
 import sanchez.juan.mydigimind.databinding.FragmentDashboardBinding
+import sanchez.juan.mydigimind.ui.Task
+import sanchez.juan.mydigimind.ui.home.HomeFragment
+import java.text.SimpleDateFormat
 
 class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var storage: FirebaseFirestore
+    private lateinit var usuario: FirebaseAuth
 
     private var _binding: FragmentDashboardBinding? = null
 
@@ -28,10 +44,82 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+
+        val btn_time : Button = root.findViewById(R.id.btn_time)
+
+
+        btn_time.setOnClickListener{
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener{timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+
+                btn_time.text = SimpleDateFormat("HH:mm").format(cal.time)
+
+            }
+
+            TimePickerDialog(root.context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE), true).show()
         }
+
+
+        val btn_save = root.findViewById(R.id.btn_save) as Button
+        val et_titulo = root.findViewById(R.id.et_task) as EditText
+        val checkMonday = root.findViewById(R.id.checkMonday) as CheckBox
+        val checkTuesday = root.findViewById(R.id.checkTuesday) as CheckBox
+        val checkWednesday = root.findViewById(R.id.checkWednesday) as CheckBox
+        val checkThursday = root.findViewById(R.id.checkThursday) as CheckBox
+        val checkFriday = root.findViewById(R.id.checkFriday) as CheckBox
+        val checkSaturday = root.findViewById(R.id.checkSatuday) as CheckBox
+        val checkSunday = root.findViewById(R.id.checkSunday) as CheckBox
+
+        btn_save.setOnClickListener{
+
+            var titulo = et_titulo.text.toString()
+            var tiempo = btn_time.text.toString()
+            var days = ArrayList<String>()
+
+            if(checkMonday.isChecked)
+                days.add("Monday")
+            if(checkTuesday.isChecked)
+                days.add("Tuesday")
+            if(checkWednesday.isChecked)
+                days.add("Wednesday")
+            if(checkThursday.isChecked)
+                days.add("Thursday")
+            if(checkFriday.isChecked)
+                days.add("Friday")
+            if(checkSaturday.isChecked)
+                days.add("Saturday")
+            if(checkSunday.isChecked)
+                days.add("Sunday")
+
+
+            val actividad = hashMapOf(
+                "actividad" to et_titulo.text.toString(),
+                "email" to usuario.currentUser?.email.toString(),
+                "lu" to checkMonday.isChecked,
+                "ma" to checkTuesday.isChecked,
+                "mi" to checkWednesday.isChecked,
+                "ju" to checkThursday.isChecked,
+                "vi" to checkFriday.isChecked,
+                "sa" to checkSaturday.isChecked,
+                "do" to checkSunday.isChecked,
+                "tiempo" to btn_time.text.toString()
+            )
+
+            storage.collection("actividades")
+                .add(actividad)
+                .addOnSuccessListener {
+                    Toast.makeText(root.context, "Task agregada", Toast.LENGTH_SHORT).show()
+                    var task = Task(titulo, days, tiempo)
+                    HomeFragment.task.add(task)
+                }
+                .addOnFailureListener{
+                    Toast.makeText(root.context, it.toString(), Toast.LENGTH_SHORT).show()
+                }
+        }
+
         return root
     }
 
